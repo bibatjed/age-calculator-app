e96d3f098e33db6284c345d10568068fa24e96cc
bibatjed <bibatjed@gmail.com>
2023-04-25 19:51:23 +0800

    age calculator functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,113 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { ChangeEvent, KeyboardEvent, useState } from "react";
 import "./App.css";
+import IconArrowSource from "./assets/images/icon-arrow.svg";
 
+type Date = {
+  day: string;
+  month: string;
+  year: string;
+};
 function App() {
-  const [count, setCount] = useState(0);
+  const [date, setDate] = useState<Date>({
+    day: "",
+    month: "",
+    year: "",
+  });
 
+  const [age, setAge] = useState<Date>({
+    day: "",
+    month: "",
+    year: "",
+  });
+  const [error, setError] = useState<boolean>(false);
+  console.log("rendering");
+
+  const onKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (!/[0-9]/.test(e.key) && e.key !== "Backspace" && e.key !== "Tab") {
+      e.preventDefault();
+    }
+  };
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setError(false);
+    setDate((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onClick = () => {
+    const birthday = new Date(`${date.month} ${date.day} ${date.year}`);
+    if (birthday.toString() == "Invalid Date") {
+      setError(true);
+      return;
+    }
+    const currentDate = new Date();
+
+    const diffTime = Math.abs(Number(currentDate) - Number(birthday));
+    const diffYears = Math.floor(diffTime / (1000 * 60 * 60 * 24 * 365));
+    const diffMonths = Math.floor(
+      (diffTime % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24 * 30)
+    );
+    const diffDays = Math.floor(
+      (diffTime % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24)
+    );
+
+    setAge({
+      day: diffDays.toString(),
+      month: diffMonths.toString(),
+      year: diffYears.toString(),
+    });
+  };
   return (
-    <>
+    <main>
+      {/* Input */}
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <span>Day</span>
+        <input
+          onKeyDown={onKeyPress}
+          type="number"
+          name="day"
+          onChange={onChange}
+          value={date.day}
+          placeholder="DD"
+        />
       </div>
-      <h1 className="text-c-purple">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+        <span>Month</span>
+        <input
+          onKeyDown={onKeyPress}
+          type="number"
+          name="month"
+          onChange={onChange}
+          value={date.month}
+          placeholder="MM"
+        />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div>
+        <span>Year</span>
+        <input
+          onKeyDown={onKeyPress}
+          name="year"
+          onChange={onChange}
+          value={date.year}
+          placeholder="YYYY"
+        />
+      </div>
+      <button
+        type="button"
+        className="bg-c-purple flex items-center justify-center rounded-full p-5"
+        onClick={onClick}
+      >
+        {" "}
+        <img src={IconArrowSource} className="w-8 h-8" />
+      </button>
+      <div>
+        <div>{age.year || "--"} years</div>
+        <div>{age.month || "--"} months</div>
+        <div>{age.day || "--"} days</div>
+      </div>
+    </main>
   );
 }
 
---
+++ src/icons/IconArrow.tsx
@@ -0,0 +1,15 @@
+export default function IconArrow() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="46"
+      height="44"
+      viewBox="0 0 46 44"
+      className="bg-c-purple flex justify-center items-center p-5 rounded-full"
+    >
+      <g fill="none" stroke="white" stroke-width="2" className="w-8 h-8">
+        <path d="M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 22.019C37.667 21.686 23 25.616 23 44" />
+      </g>
+    </svg>
+  );
+}
